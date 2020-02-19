9b8fb0c28409b14d2a5a3885e2266475034ca0ce
Eliezer <eliezerbwana21@gmail.com>
2020-02-19 17:19:01 +0200

    Ajoutez des fichiers projet.

---
+++ Boostrap/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boostrap.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+            //default controller
+        }
+
+        //Get/Home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]//home/Films
+        public IActionResult Films()
+        {
+            return View();
+        }
+
+        [HttpGet]//GEt/Home/Habillements
+        public IActionResult Habillements()
+        {
+            return View();
+        }
+
+        [HttpGet]//Get/Home/Tourisme
+        public IActionResult Tourisme()
+        {
+            return View();
+        }
+    }
+}
